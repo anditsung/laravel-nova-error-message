333dea00246ca43dabfc302b88b862ecab5c624f
anditsung <anditsung@gmail.com>
2019-10-31 12:02:45 +0700

    tambahkan constructor untuk hide dari index dan detail view

--- src/ErrorMessage.php
+++ src/ErrorMessage.php
@@ -13,5 +13,19 @@ class ErrorMessage extends Field
      */
     public $component = 'error-message';
 
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
 
+        $this->hideFromIndex();
+        $this->hideFromDetail();
+    }
 }
